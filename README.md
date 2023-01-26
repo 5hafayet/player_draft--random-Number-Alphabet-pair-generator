032502228da311082470429f615a0b7b36a944e9
shafayet <shafayetahmad1@gmail.com>
2023-01-26 11:58:17 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # player_draft--random-Number-Alphabet-pair-generator
+
 this program can be used to generate random Number-Alphabet Pair
 
-ex: 
+ex:
 
-1 J
-2 F
-3 G
-7 B
-9 E
-4 C
-8 D
-10 I
-5 H
-6 A
+3-A  
+5-D  
+6-B  
+2-C  
+4-E  
+1-F
--- player_draft.cpp
+++ player_draft.cpp
@@ -41,7 +41,7 @@ int main()
 
             for (int i = 0; i < pairNum; i++)
             {
-                cout << player[i] << " " << nameOfTeam[i] << endl;
+                cout << player[i] << "-" << nameOfTeam[i] << endl;
                 player[i] = team[i] = nameOfTeam[i] = 0;
             }
         }
